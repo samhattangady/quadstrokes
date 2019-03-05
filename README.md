ff0a20ab603330a568c46efdc0691a43853e8c7b
Samarth Hattangady <samhattangady@gmail.com>
2019-03-05 21:07:36 +0530

    feat: all working fine =)

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# Brushstrokes
+# Qaudstrokes
 
-Brushstrokes is a project to convert images into simpler geometric shapes.
-It has been inspired by the [work of fogleman's primitive](https://github.com/fogleman/primitive/), and has been implemented in rust.
+Qaudstrokes is a project to simplify images by estimating average colour.
+It has been inspired by the [work of fogleman's quads](https://github.com/fogleman/quads/), and has been implemented in rust.
 
-<img src='https://raw.githubusercontent.com/samhattangady/brushstrokes/master/docs/original_clr.jpg?sanitize=true&raw=true' /> <img src='https://raw.githubusercontent.com/samhattangady/brushstrokes/master/docs/brushstroke_clr.jpg?sanitize=true&raw=true' />
+<img src='https://raw.githubusercontent.com/samhattangady/quadstrokes/master/docs/girl_with_pearl.gif?sanitize=true&raw=true' />
 
 Currently very early stages. Results ain't great. Very slow. And lots of hardcoding. ¯\\\_(ツ)\_/¯
 
 Improvements:
 1. Use multiple threads.
 2. Improve efficiency overall.
-3. Add support for triangles, circles, etc.
-4. Use multiple starting points for hill-climb.
-5. Try out other optimisation algorithms.
-6. Add command line arguments, and other usability improvements.
+3. Add command line arguments, and other usability improvements.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,152 @@
 extern crate image;
-extern crate rand;
-extern crate rayon;
 
 use image::{GenericImage, GenericImageView};
-use rand::prelude::*;
-use rayon::prelude::*;
 use std::time::{SystemTime};
+
+#[derive(Debug, Clone, Copy)]
+struct Quadrant {
+    x: u32,
+    y: u32,
+    height: u32,
+    width: u32,
+    score: f32,
+}
 
 fn main() {
     let src = image::open("images/girl_with_pearl.jpg").unwrap();
+    let dim = src.dimensions();
     let mut dest = create_average_background_image(&src);
-    for i in 0..100 {
+    let mut quads = Vec::new();
+    let full_image_quad = Quadrant { x: 0, y: 0, width: dim.0, height: dim.1, score: -1.0 };
+    quads.push(full_image_quad);
+    let colour = get_quad_average_colour(&src, &full_image_quad);
+    for i in 0..200 {
         let now = SystemTime::now();
-        println!("Drawing shape number {:?}", i+1);
-        dest = add_best_shape(&dest, &src);
-        dest.save(format!("images/girl_iter3_step{:?}.jpg", i+1)).unwrap();
-        println!("time for step: {:?}\n", now.elapsed());
+        dest = break_up_worst_quad(&dest, &src, &mut quads);
+        draw_outlines(&dest, &quads, colour).save(format!("images/girl_with_pearl_step{:?}.jpg", i+1)).unwrap();
+        println!("time for step {:?}: {:?}\tnum of quads: {:?}", i+1, now.elapsed(), quads.len());
     }
+}
+
+fn break_up_worst_quad(dest: &image::DynamicImage, src: &image::DynamicImage, quads: &mut Vec<Quadrant>) -> image::DynamicImage {
+    calculate_all_scores(dest, src, quads);
+    let mut img = dest.clone();
+    let worst_index = get_worst_quadrant(quads);
+    let quad = quads.remove(worst_index);
+    // If height/width is odd, we get weird banding left over
+    // So that needs to be taken care of
+    let mut w1 = quad.width/2;
+    let w2 = quad.width/2;
+    if quad.width%2 != 0 {
+        w1 += 1;
+    }
+    let mut h1 = quad.height/2;
+    let h2 = quad.height/2;
+    if quad.height%2 != 0 {
+        h1 += 1;
+    }
+    let quad1 = Quadrant {
+        x: quad.x, y: quad.y,
+        width: w1, height: h1,
+        score: -1.0,
+    };
+    img = fix_quadrant(&img, src, quad1);
+    quads.push(quad1);
+    let quad2 = Quadrant {
+        x: quad.x+w1, y: quad.y,
+        width: w2, height: h1,
+        score: -1.0,
+    };
+    img = fix_quadrant(&img, src, quad2);
+    quads.push(quad2);
+    let quad3 = Quadrant {
+        x: quad.x+w1, y: quad.y+h1,
+        width: w2, height: h2,
+        score: -1.0,
+    };
+    img = fix_quadrant(&img, src, quad3);
+    quads.push(quad3);
+    let quad4 = Quadrant {
+        x: quad.x, y: quad.y+h1,
+        width: w1, height: h2,
+        score: -1.0,
+    };
+    img = fix_quadrant(&img, src, quad4);
+    quads.push(quad4);
+    img
+}
+
+fn fix_quadrant(dest: &image::DynamicImage, src: &image::DynamicImage, quadrant: Quadrant) -> image::DynamicImage {
+    let colour = get_quad_average_colour(src, &quadrant);
+    set_quad_average_colour(dest, &quadrant, colour)
+}
+
+fn get_worst_quadrant(quads: &mut Vec<Quadrant>) -> usize {
+    // Iterate through all the quads, find their scores
+    // and return the index of the worst quad.
+    let mut worst_index = 0;
+    let mut worst_score = quads[0].score;
+    for i in 1..quads.len() {
+        if quads[i].score > worst_score {
+            worst_index = i;
+            worst_score = quads[i].score
+        }
+    }
+    worst_index
+}
+
+fn calculate_all_scores(dest: &image::DynamicImage, src: &image::DynamicImage, quads: &mut Vec<Quadrant>) {
+    for quadrant in quads {
+        if quadrant.score < 0.0 {
+            quadrant.score = calculate_quadrant_score(&quadrant, dest, src);
+        }
+    }
+}
+
+fn get_quad_average_colour(src: &image::DynamicImage, quadrant: &Quadrant) -> [u8; 3] {
+    let mut avg_red = 0;
+    let mut avg_green = 0;
+    let mut avg_blue = 0;
+    for x in quadrant.x..quadrant.x+quadrant.width {
+        for y in quadrant.y..quadrant.y+quadrant.height {
+            let p1 = src.get_pixel(x, y);
+            avg_red += p1[0] as u32;
+            avg_green += p1[1] as u32;
+            avg_blue += p1[2] as u32;
+        }
+    }
+    let total_pixels = quadrant.width*quadrant.height;
+    [(avg_red/total_pixels) as u8, (avg_green/total_pixels) as u8, (avg_blue/total_pixels) as u8]
+}
+
+fn set_quad_average_colour(dest: &image::DynamicImage, quadrant: &Quadrant, color: [u8; 3]) -> image::DynamicImage {
+    let mut img = dest.clone();
+    for x in quadrant.x..quadrant.x+quadrant.width {
+        for y in quadrant.y..quadrant.y+quadrant.height {
+            img.put_pixel(x, y, image::Rgba{data: [color[0] as u8, color[1] as u8, color[2] as u8, 255]});
+        }
+    }
+    img
+}
+
+fn calculate_quadrant_score(quadrant: &Quadrant, dest: &image::DynamicImage, src: &image::DynamicImage) -> f32 {
+    let mut square_error = 0.0;
+    for x in quadrant.x..quadrant.x+quadrant.width {
+        for y in quadrant.y..quadrant.y+quadrant.height {
+            let p1 = dest.get_pixel(x, y);
+            let p2 = src.get_pixel(x, y);
+            let r1 = p1[0] as f32;
+            let g1 = p1[1] as f32;
+            let b1 = p1[2] as f32;
+            let r2 = p2[0] as f32;
+            let g2 = p2[1] as f32;
+            let b2 = p2[2] as f32;
+            square_error += (r2-r1).powf(2.0);
+            square_error += (g2-g1).powf(2.0);
+            square_error += (b2-b1).powf(2.0);
+        }
+    }
+    square_error.powf(0.5)
 }
 
 fn create_average_background_image(src: &image::DynamicImage) -> image::DynamicImage {
@@ -49,140 +179,23 @@ fn create_average_background_image(src: &image::DynamicImage) -> image::DynamicI
     dest
 }
 
-fn add_best_shape(img: &image::DynamicImage, src: &image::DynamicImage) -> image::DynamicImage {
-    // Hill climb algo scammed from wikipedia
-    // Don't know what epsilon is though
-    let image_width = src.dimensions().0 as usize;
-    let src_pixels = image_to_vector(src);
-    let entry_pixels = image_to_vector(img);
-
-    // TODO (03 Mar 2019 sam): step_sizes is hardocoded. Should ideally be dynamically created
-    // and equal in length to current_shape, and possibly with different values based on
-    // what is being optimised
-    // NOTE (03 Mar 2019 sam): step_sizes[7] is alpha, which should be between 0.0 and 1.0
-    let mut step_sizes = [10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 0.5];
-    let acceleration = 1.2;
-    let candidates = [-acceleration, -1.0/acceleration, 0.0, 1.0/acceleration, acceleration];
-    let mut current_shape = get_start_point(&img);
-    let mut best_score = std::i32::MAX as f32;
-    let mut step_score;
-
-    for _ in 0..100 {
-        // let now = SystemTime::now();
-        step_score = best_score;
-        best_score = std::i32::MAX as f32;
-        for i in 0..current_shape.len() {
-            let mut best = 10;
-            best_score = std::i32::MAX as f32;
-            for j in 0..candidates.len() {
-                let temp = (step_sizes[i] * candidates[j]) as i32;
-                current_shape[i] = current_shape[i] + temp;
-                let current_pixels = draw_shape(current_shape, &entry_pixels, image_width);
-                let current_score = get_rmse(&current_pixels, &src_pixels);
-                current_shape[i] = current_shape[i] - (step_sizes[i] * candidates[j]) as i32;
-                if current_score < best_score {
-                    best_score = current_score;
-                    best = j;
-                }
-            }
-            if candidates[best] == 0.0 {
-                step_sizes[i] = step_sizes[i]/acceleration;
-            } else {
-                current_shape[i] = current_shape[i] + (step_sizes[i] * candidates[best]) as i32;
-                step_sizes[i] = step_sizes[i] * candidates[best];
-            }
+fn draw_outlines(dest: &image::DynamicImage, quads: &Vec<Quadrant>, colour: [u8; 3]) -> image::DynamicImage {
+    let mut img = dest.clone();
+    for quadrant in quads {
+        for x in quadrant.x..quadrant.x+quadrant.width {
+            img.put_pixel(x, quadrant.y, image::Rgba{data: [colour[0], colour[1], colour[2], 255]});
         }
-        // println!("computing for best shape took : {:?}\n", now.elapsed());
-        // TODO (24 Feb 2019 sam) Figure out what this epsilon value is supposed to be
-        if (step_score - best_score) < 0.000005 {
-            break;
+        for y in quadrant.y..quadrant.y+quadrant.height {
+            img.put_pixel(quadrant.x, y, image::Rgba{data: [colour[0], colour[1], colour[2], 255]});
         }
     }
-    let final_img = draw_shape(current_shape, &entry_pixels, image_width);
-    // NOTE(24 Feb 2019 sam): We might want to check the score here to make sure its improved
-    // Currently it is assumed that it is improved
-    vector_to_image(final_img, src)
-}
-
-fn get_start_point(img: &image::DynamicImage) -> [i32; 8] {
-    // generate random start point
-    let image_width = img.dimensions().0;
-    let image_height = img.dimensions().1;
-
-    let mut rng = rand::thread_rng();
-    // NOTE (20 Feb 2019 sam): Keeping color as i32 for ease of code. Might need to constrain it somewhere
-    // though logically, no optimisation method should bother going outside the range
-    // PS. Note about constraints applies to all the variables...
-    let x1: i32 = rng.gen_range(0, image_width as i32);
-    let y1: i32 = rng.gen_range(0, image_height as i32);
-    let x2: i32 = rng.gen_range(0, image_width as i32);
-    let y2: i32 = rng.gen_range(0, image_height as i32);
-    let red: i32 = rng.gen_range(0, 255);
-    let green: i32 = rng.gen_range(0, 255);
-    let blue: i32 = rng.gen_range(0, 255);
-    let alpha : i32 = rng.gen_range(0, std::i32::MAX);
-    [x1, y1, x2, y2, red, green, blue, alpha]
-}
-
-fn compute_rmse(p1: [u8; 3], p2: [u8; 3]) -> f32 {
-    // TODO (24 Feb 2019 sam) See how this can be cleaned up
-    // let (p1, p2) = pixels;
-    let mut square_error = 0.0;
-    let r1 = p1[0] as f32;
-    let g1 = p1[1] as f32;
-    let b1 = p1[2] as f32;
-    let r2 = p2[0] as f32;
-    let g2 = p2[1] as f32;
-    let b2 = p2[2] as f32;
-    square_error += (r2-r1).powf(2.0);
-    square_error += (g2-g1).powf(2.0);
-    square_error += (b2-b1).powf(2.0);
-    square_error
-}
-
-fn get_rmse(img1: &Vec<[u8; 3]>, img2: &Vec<[u8; 3]>) -> f32 {
-    // FIXME: (03 Mar 2019 sam): par_iter is slower than iter. See why that could be
-    // let mut square_error:f32 = img1.par_iter()
-    //                                .zip(img2.par_iter())
-    //                                .map(|(p1, p2)| compute_rmse(*p1, *p2))
-    //                                .sum();
-    let mut square_error:f32 = img1.iter()
-                                   .zip(img2.iter())
-                                   .map(|(p1, p2)| compute_rmse(*p1, *p2))
-                                   .sum();
-    square_error /= 3.0 * img1.len() as f32;
-    square_error.powf(0.5)
-}
-
-fn draw_shape(shape: [i32;8], img: &Vec<[u8; 3]>, image_width: usize) -> Vec<[u8; 3]> {
-    let mut new_pixels = img.clone();
-    let image_height = img.len() / image_width;
-
-    let minx = std::cmp::min(shape[0], shape[2]) as usize;
-    let miny = std::cmp::min(shape[1], shape[3]) as usize;
-    let mut maxx = std::cmp::max(shape[0], shape[2]) as usize;
-    let mut maxy = std::cmp::max(shape[1], shape[3]) as usize;
-    let red = shape[4] as u8;
-    let green = shape[5] as u8;
-    let blue = shape[6] as u8;
-    let alpha = shape[7] as f32 / std::i32::MAX as f32;
-    // contstraining shape
-    if maxx >= image_width { maxx = image_width-1; }
-    if maxy >= image_height { maxy = image_height-1; }
-
-    // draw the shape
-    for x in minx..maxx+1 {
-        for y in miny..maxy+1 {
-            let old_red = img[y as usize*image_width + x][0];
-            let new_red = ((red as f32 * alpha) + old_red as f32 * (1.0-alpha)) as u8;
-            let old_green = img[y as usize*image_width + x][1];
-            let new_green = ((green as f32 * alpha) + old_green as f32 * (1.0-alpha)) as u8;
-            let old_blue = img[y as usize*image_width + x][2];
-            let new_blue = ((blue as f32 * alpha) + old_blue as f32 * (1.0-alpha)) as u8;
-            new_pixels[y as usize*image_width + x] = [new_red, new_green, new_blue];
-        }
+    for x in 0..img.dimensions().0 {
+        img.put_pixel(x, img.dimensions().1-1, image::Rgba{data: [colour[0], colour[1], colour[2], 255]});
     }
-    new_pixels
+    for y in 0..img.dimensions().1 {
+        img.put_pixel(img.dimensions().0-1, y, image::Rgba{data: [colour[0], colour[1], colour[2], 255]});
+    }
+    img
 }
 
 fn image_to_vector(image: &image::DynamicImage) -> Vec<[u8; 3]> {
@@ -191,17 +204,4 @@ fn image_to_vector(image: &image::DynamicImage) -> Vec<[u8; 3]> {
         pixels.push([pixel.2[0], pixel.2[1], pixel.2[2]]);
     }
     pixels
-}
-
-fn vector_to_image(vector: Vec<[u8; 3]>, src: &image::DynamicImage) -> image::DynamicImage {
-    let mut img = src.clone();
-    let width = img.dimensions().0 as usize;
-    let height = img.dimensions().1 as usize;
-    for x in 0..width {
-        for y in 0..height {
-            let pixel = vector[y*width + x];
-            img.put_pixel(x as u32, y as u32, image::Rgba{ data:[pixel[0], pixel[1], pixel[2], 255] });
-        }
-    }
-    img
 }
